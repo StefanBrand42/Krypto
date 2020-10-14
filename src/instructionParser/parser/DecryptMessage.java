eafe4d34d0d6b37d1ac1babdabc09a7b27250f15
StefanBrand42 <56312367+StefanBrand42@users.noreply.github.com>
2020-10-14 20:01:15 +0200

    decrypt Message Fehler eingabe abgefangen

--- src/instructionParser/parser/DecryptMessage.java
+++ src/instructionParser/parser/DecryptMessage.java
@@ -1,5 +1,6 @@
 package instructionParser.parser;
 
+import configuration.Configuration;
 import crypto.CryptoCreator;
 import gui.GUI;
 import instructionParser.ParserInstruction;
@@ -23,8 +24,33 @@ public class DecryptMessage extends ParserInstruction {
                 String algo = commandLineArray[4];
                 String key = commandLineArray[7];
 
-                gui.getCryptoCreator().setLogging(new Logging("decrypt",algo));
+
+            // Check if Algo and KeyFile exist in Components
+            boolean readyforEncrypt = true  ;
+            StringBuilder stringBuilder01 = new StringBuilder();
+            if (!Configuration.instance.checkIfAlgoExist(algo)){
+                stringBuilder01.append("Your chosen algorithm does not exist. \n");
+                readyforEncrypt = false;
+            }
+
+            if (!Configuration.instance.checkIfKeyFileNameExist(key)){
+                stringBuilder01.append("Your chosen keyfile does not exist. (please write it without .json) \n");
+
+                readyforEncrypt = false;
+            }
+
+            if (readyforEncrypt)
+            {
+                if (Configuration.instance.debug){
+                    gui.getCryptoCreator().setLogging(new Logging("decrypt",algo));
+                }
+
                 gui.writeTextAreaGui(gui.getCryptoCreator().decryptMessage(message,algo,key));
+            }else {
+                gui.writeTextAreaGui(stringBuilder01.toString());
+            }
+
+
         }else{
             super.parse(commandLine,gui);
         }
